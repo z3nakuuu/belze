0e1139a7a86308bbed32d563f23b4c92a4434783
z3naku <sandovalolguin.c@gmail.com>
2026-04-20 09:12:38 -0400

    entrenador="takato"
    dign1=6
    
    entrenador2="ruki"
    dign2=7
    
    if dign2<=6:
        print("Puede acceder a la liga digimon")
    else:
        print("tiene mas digimons de los necesarios")

---
+++ ffff.py
@@ -0,0 +1,12 @@
+entrenador="takato"
+
+dign1=6
+
+entrenador2="ruki"
+dign2=7
+
+if dign2<=6:
+    print("Puede acceder a la liga digimon")
+else:
+    print("tiene mas digimons de los necesarios")
+
