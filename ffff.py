931466492654027b5db6890ca47d722168c430f2
z3naku <sandovalolguin.c@gmail.com>
2026-04-20 09:40:14 -0400

    concadenacioones

--- ffff.py
+++ ffff.py
@@ -5,8 +5,16 @@ dign1=6
 entrenador2="ruki"
 dign2=7
 
-if dign2<=6:
-    print("Puede acceder a la liga digimon")
-else:
-    print("tiene mas digimons de los necesarios")
+#if dign2<=6:
+#    print("Puede acceder a la liga digimon")
+#else:
+ #   print("tiene mas digimons de los necesarios")
+
+
+# print(len(entrenador))
+
+print("El entrenador", entrenador, "tiene", dign1, "digimons")
+print("El entrenador", entrenador2 "tiene", dign2, "digimons")
+print(f"El entrenador {entrenador2} tiene {dign1} digimons")
+
 
