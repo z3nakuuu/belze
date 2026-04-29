59c28eaaba30572414b8ae6f2caf49335bf8039f
z3nakuuu <sandovalolguin.c@gmail.com>
2026-04-29 16:29:04 -0400

    rep

--- ejerciciosparala.py
+++ ejerciciosparala.py
@@ -1,5 +1,8 @@
 op=0
 cantpersonas=0
+cantniños=0
+cantadultos=0
+cantviejitos=0
 total=0
 while op!=4:
     print('''
@@ -11,18 +14,26 @@ while op!=4:
     match op:
         case 1:
             print("Pagando el precio de niño")
+            print(f"la cantidad de niños es {cantniños}")
             cantpersonas+=1
+            cantniños+=1
             total+=1000
         case 2:
             print("pagando el precio de adulto")
+            print(f"la cantidad de adultos es {cantadultos}")
             cantpersonas+=1
+            cantadultos+=1
             total+=3000
         case 3:
             print("pagando el precio adulto mayor")
+            print(f"la cantidad de adultos mayores es {cantviejitos}")
             cantpersonas+=1
+            cantviejitos+=1
             total+=1500
         case 4:
             print("Saliendo del menu")
+            print(f"el total a pagar es {total}")
+            print(f"la cantidad de personas es { cantpersonas}")
             op=4
         case _:
             print("Opción inválida")
